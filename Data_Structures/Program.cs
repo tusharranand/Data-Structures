a694994a4e15804083bb5ee4ecf9a51fbd17d64a
Tushar Anand <tusharranand@gmail.com>
2022-03-23 11:07:38 +0530

    [Tushar Anand] Added: Introduction

---
+++ Data_Structures/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Data_Structures
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Data Structures program.");
+        }
+    }
+}
