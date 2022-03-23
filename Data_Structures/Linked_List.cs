de98856cf9ec9e56922b56a4a15d6a735c98dbcc
Tushar Anand <tusharranand@gmail.com>
2022-03-23 13:07:10 +0530

    [Tushar Anand] Added: Ability to add nodes

--- Data_Structures/Linked_List.cs
+++ Data_Structures/Linked_List.cs
@@ -16,5 +16,42 @@ namespace Data_Structures
             Head = null;
             Tail = null;
         }
+
+        public void AddNode(Node node)
+        {
+            if (Head == null && Tail == null)
+            {
+                Head = node;
+                Tail = node;
+            }
+            else
+            {
+                node.Next = Head;
+                Head = node;
+            }
+
+        }
+        public void Display()
+        {
+            Node temp = Head;
+
+            if (temp == null)
+            {
+                Console.WriteLine("Linked List is empty.");
+            }
+            else
+            {
+                Console.WriteLine("Linked List is as follows");
+                while (temp != null)
+                {
+                    Console.Write(temp.Data);
+                    if (temp.Next != null)
+                    {
+                        Console.Write(" -> ");
+                    }
+                    temp = temp.Next;
+                }
+            }
+        }
     }
 }
--- Data_Structures/Program.cs
+++ Data_Structures/Program.cs
@@ -12,8 +12,16 @@ namespace Data_Structures
         {
             Console.WriteLine("Welcome to Data Structures program.");
 
-            Linked_List linked_List = new Linked_List();
+            Linked_List linked_list = new Linked_List();
             Node node1 = new Node(22);
+            Node node2 = new Node(23);
+            Node node3 = new Node(24);
+
+            linked_list.AddNode(node1);
+            linked_list.AddNode(node2);
+            linked_list.AddNode(node3);
+
+            linked_list.Display();
         }
     }
 }
