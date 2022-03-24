6e8da29b7616424f3e1ece68365a827046b1d3b4
tusharranand <99305968+tusharranand@users.noreply.github.com>
2022-03-24 16:01:13 +0530

    Update Program.cs

--- Data_Structures/Program.cs
+++ Data_Structures/Program.cs
@@ -13,20 +13,20 @@ namespace Data_Structures
             Console.WriteLine("Welcome to Data Structures program.");
 
             Linked_List linked_list = new Linked_List();
-            Node node1 = new Node(22);
-            Node node2 = new Node(23);
-            Node node3 = new Node(24);
-            linked_list.AddNode(node1);
-            linked_list.AddNode(node2);
-            linked_list.AddNode(node3);
+            Node node_one = new Node(22);
+            Node node_two = new Node(23);
+            Node node_three = new Node(24);
+            linked_list.AddNode(node_one);
+            linked_list.AddNode(node_two);
+            linked_list.AddNode(node_three);
             linked_list.Display();
 
-            Node node4 = new Node(21);
-            linked_list.AppendNode(node4);
+            Node node_four = new Node(21);
+            linked_list.AppendNode(node_four);
             linked_list.Display();
 
-            Node node5 = new Node(25);
-            linked_list.AddNodeatStart(node5);
+            Node node_five = new Node(25);
+            linked_list.AddNodeatStart(node_five);
             linked_list.Display();
         }
     }
